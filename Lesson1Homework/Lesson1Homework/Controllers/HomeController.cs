f1b3b4aac706cc889f01e7fc9505b9567ebedf0f
Unknown <vovanpadawan@gmail.com>
2018-09-06 09:57:46 +0300

    Сделал ДЗ 1го урока

---
+++ Lesson1Homework/Lesson1Homework/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lesson1Homework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> list = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Имя 1",
+                Patronymic = "Отчество 1",
+                Surname = "Фамилия 1",
+                Age = 11
+            },
+
+          new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Имя 2",
+                Patronymic = "Отчество 2",
+                Surname = "Фамилия 2",
+                Age = 22
+            },
+
+          new EmployeeView
+            {
+                Id = 3,
+                FirstName = "Имя 3",
+                Patronymic = "Отчество 3",
+                Surname = "Фамилия 3",
+                Age = 33
+            },
+
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Имя 4",
+                Patronymic = "Отчество 4",
+                Surname = "Фамилия 4",
+                Age = 44
+            },
+
+        };
+
+        public IActionResult Index()
+        {
+            //return Content("Controller");
+            return View(list);
+        }
+
+        public IActionResult Details (int id)
+        {
+
+            return View(list[id]);
+        }
+    }
+}
---
+++ Lesson1Homework/Lesson1Homework/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lesson1Homework
+{
+    public class Startup
+    {
+        //используем автосвойство
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //использование конфигов и другого
+            app.UseStaticFiles();
+
+            //var test = Configuration["CustomKey"];
+            
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(test);
+            //});
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            }
+                );
+        }
+    }
+}
