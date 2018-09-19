37688c08c52435da7f22b23a76bd8697d8154d35
Unknown <vovanpadawan@gmail.com>
2018-09-19 23:15:53 +0300

    Добавил класс продукта, добавил Microsoft.EntityFrameworkCore.SqlServer последней версии, 2.0 чёт не ставился

--- Lesson3Homework/Homework.Domain/Models/Brand.cs
+++ Lesson3Homework/Homework.Domain/Models/Brand.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Homework.Domain.Models
 {
+    [Table("Brands")]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get ; set ; }
--- Lesson3Homework/Homework.Domain/Models/Section.cs
+++ Lesson3Homework/Homework.Domain/Models/Section.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Homework.Domain.Models
 {
+    [Table("Sections")]
     public class Section: NamedEntity, IOrderedEntity
     {
+        [ForeignKey("ParentId")]
         public int? ParentId { get; set; }
 
         public int Order { get; set; }
