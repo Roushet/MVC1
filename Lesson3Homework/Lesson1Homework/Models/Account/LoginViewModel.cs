e18c796fa4e63aa999524bef906e7f68a2e9860e
Vladimir Evdokimov <vovanpadawan@gmail.com>
2018-09-27 12:25:00 +0300

    Сделал таки домашку за прошлый урок

---
+++ Lesson3Homework/Lesson1Homework/Models/Account/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+
+namespace Lesson1Homework.Models.Account
+{
+    //Наколхозил модель, которой не было в методичке
+    public class LoginViewModel : RegisterUserViewModel
+    {
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; internal set; }
+    }
+}
